aaa8f38521bf569ef4f37a82a5ae05fff926d164
Santhosh V <santhosh.v6426@gmail.com>
2020-08-31 13:52:07 +0530

    java code

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+
+public class helloworld
+{
+	public static void main(String [] argv)
+	{
+		while(true)
+		{
+			System.out.println("helloworld");
+			try
+			{
+				Thread.sleep(6000);
+			}
+			catch(Exception ex)
+			{
+
+			}
+		}	
+	}
+}
